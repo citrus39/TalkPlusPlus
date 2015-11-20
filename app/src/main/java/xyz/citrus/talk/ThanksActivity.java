12f644cd2d0b1135be07b7e574b8693032f2601f
citrus39 <antonov.nikky@hotmail.com>
2015-11-20 20:21:08 +0200

    commit

---
+++ app/src/main/java/xyz/citrus/talk/ThanksActivity.java
@@ -0,0 +1,13 @@
+package xyz.citrus.talk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanks);
+    }
+}
